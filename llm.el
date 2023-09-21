9ca27d742b43283f5c81d925be74dc0d27c95924
Andrew Hyatt <ahyatt@gmail.com>
2023-09-21 00:37:19 -0400

    Bumping the version to 0.1.1 including FSF copyright and style fixes

--- llm.el
+++ llm.el
@@ -5,7 +5,7 @@
 ;; Author: Andrew Hyatt <ahyatt@gmail.com>
 ;; Homepage: https://github.com/ahyatt/llm
 ;; Package-Requires: ((request "0.3.3") (emacs "28.1"))
-;; Package-Version: 0.1
+;; Package-Version: 0.1.1
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
 ;; This program is free software; you can redistribute it and/or
