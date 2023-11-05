a26abb4edee6c9f125c1c10804f197795be7fc6c
Andrew Hyatt <ahyatt@gmail.com>
2023-11-04 23:58:17 -0400

    Bump version to 0.5.2

--- llm.el
+++ llm.el
@@ -5,7 +5,7 @@
 ;; Author: Andrew Hyatt <ahyatt@gmail.com>
 ;; Homepage: https://github.com/ahyatt/llm
 ;; Package-Requires: ((emacs "28.1"))
-;; Package-Version: 0.5.1
+;; Package-Version: 0.5.2
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
 ;; This program is free software; you can redistribute it and/or
