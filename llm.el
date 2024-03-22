144edd99441314a88294e9c3457a42cc8351d564
Andrew Hyatt <ahyatt@gmail.com>
2024-03-22 01:02:19 -0400

    Set version to 0.12.1

--- llm.el
+++ llm.el
@@ -5,7 +5,7 @@
 ;; Author: Andrew Hyatt <ahyatt@gmail.com>
 ;; Homepage: https://github.com/ahyatt/llm
 ;; Package-Requires: ((emacs "28.1"))
-;; Package-Version: 0.12.0
+;; Package-Version: 0.12.1
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
 ;; This program is free software; you can redistribute it and/or
