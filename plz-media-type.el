c2deac60c45158b7542da1bbabef8390b9fdb712
Roman Scherer <roman@burningswell.com>
2024-04-15 17:40:35 +0200

    Add note that plz-media-type and plz-event-source are vendored

--- plz-media-type.el
+++ plz-media-type.el
@@ -9,7 +9,11 @@
 ;; Package-Requires: ((emacs "26.3"))
 ;; Keywords: comm, network, http
 
-;; This file is part of GNU Emacs.
+;; This file is NOT YET part of GNU Emacs.  It is temporarily vendored
+;; within the llm library.  Please DO NOT depend on it!  It is subject
+;; to change.  Once we think this package is stable, we will release
+;; it to GNU ELPA.  If no serious issues are found, we plan to do this
+;; in Q4 of 2024.
 
 ;;; License:
 
