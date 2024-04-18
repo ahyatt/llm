34b7ca8b0402c5e9ef555e9bb412a97d6cf65e67
Roman Scherer <roman@burningswell.com>
2024-04-18 08:29:45 +0200

    Mention plz-media-type and plz-event-source are part of Emacs

--- plz-event-source.el
+++ plz-event-source.el
@@ -9,11 +9,12 @@
 ;; Package-Requires: ((emacs "26.3"))
 ;; Keywords: comm, network, http
 
-;; This file is NOT YET part of GNU Emacs.  It is temporarily vendored
-;; within the llm library.  Please DO NOT depend on it!  It is subject
-;; to change.  Once we think this package is stable, we will release
-;; it to GNU ELPA.  If no serious issues are found, we plan to do this
-;; in Q4 of 2024.
+;; This file is part of GNU Emacs.
+
+;; It is temporarily vendored within the llm library.  Please DO NOT
+;; depend on it!  It is subject to change.  Once we think this package
+;; is stable, we will release it to GNU ELPA.  If no serious issues
+;; are found, we plan to do this in Q4 of 2024.
 
 ;;; License:
 
