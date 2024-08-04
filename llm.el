8c6fd8378214dfabff15d193713906f19da2352d
Andrew Hyatt <ahyatt@gmail.com>
2024-08-03 20:35:28 -0400

    Change version to 0.17.1
    Remove the mistaken 0.17.2 from NEWS, we never released 0.17.1, so put
    everything in that release.

--- llm.el
+++ llm.el
@@ -5,7 +5,7 @@
 ;; Author: Andrew Hyatt <ahyatt@gmail.com>
 ;; Homepage: https://github.com/ahyatt/llm
 ;; Package-Requires: ((emacs "28.1") (plz "0.8"))
-;; Package-Version: 0.17.0
+;; Package-Version: 0.17.1
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 ;;
 ;; This program is free software; you can redistribute it and/or
