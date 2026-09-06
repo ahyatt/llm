3498f46712940695ce8d2115678b7f67b51e6e78
Andrew Hyatt <ahyatt@gmail.com>
2026-09-06 04:03:47 +0200

    Throw meaningful errors on integer ports, and fix the documentation (#315)
    This fixes https://github.com/ahyatt/llm/issues/310

--- llm-gpt4all.el
+++ llm-gpt4all.el
@@ -60,6 +60,9 @@ default the default GPT4all port."
 
 (cl-defmethod llm-provider-chat-url ((provider llm-gpt4all))
   "Return the URL for PATH, given the settings in PROVIDER."
+  (when (and (llm-gpt4all-port provider)
+             (not (integerp (llm-gpt4all-port provider))))
+    (error "GPT4All port, if set, must be an integer"))
   (format "http://%s:%d/v1/chat/completions" (or (llm-gpt4all-host provider) "localhost")
           (or (llm-gpt4all-port provider) 4891)))
 
